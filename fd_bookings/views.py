9d99532e0576fdb243b61f245e593a98374198f8
Emma Cadavra <emmacadavra@gmail.com>
2023-11-07 21:36:22 +0000

    Add placeholder code to templates

--- fd_bookings/views.py
+++ fd_bookings/views.py
@@ -1,15 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-# Create your views here.
 
 def homepage(request):
-    return HttpResponse("Home Page")
+    # return HttpResponse("Home Page")
+    return render(request, 'index.html')
 
 
 def booking(request):
-    return HttpResponse("Booking Page")
+    # return HttpResponse("Booking Page")
+    return render(request, 'booking.html')
 
 
 def contact(request):
-    return HttpResponse("Contact Page")
+    # return HttpResponse("Contact Page")
+    return render(request, 'contact.html')
