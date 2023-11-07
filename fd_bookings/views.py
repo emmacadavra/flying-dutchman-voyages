eb07eec8309736647225d64a24a72d5958fc4dbf
Emma Cadavra <emmacadavra@gmail.com>
2023-11-07 21:26:38 +0000

    Add basic views

--- fd_bookings/views.py
+++ fd_bookings/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def homepage(request):
+    return HttpResponse("Home Page")
+
+
+def booking(request):
+    return HttpResponse("Booking Page")
+
+
+def contact(request):
+    return HttpResponse("Contact Page")
