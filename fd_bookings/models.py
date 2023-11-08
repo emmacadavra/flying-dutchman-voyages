554b736f085aea70f0f905bd33e5d29f87fe6ae5
Emma Cadavra <emmacadavra@gmail.com>
2023-11-08 14:43:19 +0000

    Create superuser, fix allowed hosts

--- fd_bookings/models.py
+++ fd_bookings/models.py
@@ -3,15 +3,15 @@ from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
 
 
-EVENTS = ((0, "No Event"), (1, "Seasonal Event"), (2, "Easter Sunday Event"))
+# EVENTS = ((0, "No Event"), (1, "Seasonal Event"), (2, "Easter Sunday Event"))
 
-class Booking(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    booking_date = models.DateField()
-    num_passengers = models.PositiveIntegerField()
-    created_on = models.DateTimeField(auto_now_add=True)
-    event = models.IntegerField(choices=EVENTS, default=0)
-    total_cost = models.DecimalField()
+# class Booking(models.Model):
+#     user = models.ForeignKey(User, on_delete=models.CASCADE)
+#     booking_date = models.DateField()
+#     num_passengers = models.PositiveIntegerField()
+#     created_on = models.DateTimeField(auto_now_add=True)
+#     event = models.IntegerField(choices=EVENTS, default=0)
+#     total_cost = models.DecimalField()
 
-    def __str__(self):
-        return f"Booking {self.id} created by {self.user.username} for {self.booking_date}"
+#     def __str__(self):
+#         return f"Booking {self.id} created by {self.user.username} for {self.booking_date}"
--- flying_dutchman/settings.py
+++ flying_dutchman/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['flying-dutchman-voyages-f488bfe43a6a.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['flying-dutchman-voyages-f488bfe43a6a.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
