e22b4edd85ef3deeffa3a5be638e8018da0181e4
Emma Cadavra <emmacadavra@gmail.com>
2023-11-08 12:43:27 +0000

    Reinstall Django<4 and start project again due to conflicts with previously installed version 4.1

--- fd_bookings/views.py
+++ fd_bookings/views.py
@@ -1,17 +1,3 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
-
-def homepage(request):
-    # return HttpResponse("Home Page")
-    return render(request, 'index.html')
-
-
-def booking(request):
-    # return HttpResponse("Booking Page")
-    return render(request, 'booking.html')
-
-
-def contact(request):
-    # return HttpResponse("Contact Page")
-    return render(request, 'contact.html')
+# Create your views here.
--- flying_dutchman/wsgi.py
+++ flying_dutchman/wsgi.py
@@ -1,16 +1,16 @@
 """
-WSGI config for flyingdutchman project.
+WSGI config for flying_dutchman project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 """
 
 import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'flyingdutchman.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'flying_dutchman.settings')
 
 application = get_wsgi_application()
