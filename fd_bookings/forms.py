ba266959e5e4de6e74ce26b7bd5f41a9872e2de4
Emma Cadavra <emmacadavra@gmail.com>
2023-11-10 18:13:11 +0000

    Create class AvailabilityForm including ROOM_CATEGORIES from models.Rooms

--- fd_bookings/forms.py
+++ fd_bookings/forms.py
@@ -1,2 +1,13 @@
 from django import forms
 
+
+class AvailabilityForm(forms.Form):
+    ROOM_CATEGORIES = (
+        ('CAP', 'CAPTAINS QUARTERS'),
+        ('OCD', 'DOUBLE CABIN'),
+        ('OCS', 'SINGLE CABIN'),
+        ('CRW', 'CREW BUNKS'),
+    )
+    
+    room_category = forms.ChoiceField(choices=ROOM_CATEGORIES, required=True)
+    booking_date = forms.DateField(required=True, input_formats='%d/%m/%Y')
