b01880a6f416c7a837e58af940fac0e2073e0488
Emma Cadavra <emmacadavra@gmail.com>
2023-11-10 16:54:02 +0000

    Add urlpatterns paths

--- fd_bookings/urls.py
+++ fd_bookings/urls.py
@@ -1,8 +1,9 @@
-from . import views
+from .views import RoomList, BookingList
 from django.urls import path
 
 app_name='fd_bookings'
 
 urlpatterns=[
-    
+    path('room_list/', RoomList.as_view(), name='RoomList'),
+    path('booking_list/', BookingList.as_view(), name='BookingList'),
 ]
