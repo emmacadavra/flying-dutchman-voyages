c1bfefa1387c3f3bd687e7277662da14ba99ccfb
Emma Cadavra <emmacadavra@gmail.com>
2023-11-10 18:48:14 +0000

    Code along with DarshanDev's Django HMS tutorial - temporary code

--- fd_bookings/urls.py
+++ fd_bookings/urls.py
@@ -5,5 +5,6 @@ app_name='fd_bookings'
 
 urlpatterns=[
     path('rooms/', views.RoomList.as_view(), name='rooms'),
-    path('bookings/', views.BookingList.as_view(), name='bookings'),
+    # path('bookings/', views.BookingList.as_view(), name='bookings'),
+    path('bookings/', views.BookingView.as_view(), name='bookings')
 ]
--- fd_bookings/views.py
+++ fd_bookings/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from django.views import generic, View
+from django.http import HttpResponse
 from .models import Room, Booking
 from .forms import AvailabilityForm
-from booking_functions.availability import check_availability
+from fd_bookings.booking_functions.availability import check_availability
 
 
 # Create your views here.
@@ -27,4 +28,16 @@ class BookingView(generic.FormView):
         for room in rooms:
             if check_availability(room, data['booking_date']):
                 available_rooms.append(room)
-            
+
+        if len(available_rooms)>0:
+
+            room = available_rooms[0]
+            booking = Booking.objects.create(
+                user = request.user,
+                room = room,
+                booking_date = data['booking_date'],
+            )
+            booking.save()
+            return HttpResponse(booking)
+        else:
+            return HttpResponse('This room type is not available.')
