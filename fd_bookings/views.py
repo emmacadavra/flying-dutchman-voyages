aa16f52fedc32466ccc33e944d9327bc2f2d9104
Emma Cadavra <emmacadavra@gmail.com>
2023-11-10 18:18:10 +0000

    Create ckass BookingView in views linked to AvailabilityForm

--- fd_bookings/views.py
+++ fd_bookings/views.py
@@ -1,14 +1,21 @@
 from django.shortcuts import render
 from django.views import generic, View
 from .models import Room, Booking
+from .forms import AvailabilityForm
 
 
 # Create your views here.
 class RoomList(generic.ListView):
     model = Room
-    template_name = "index.html"
+    template_name = 'index.html'
 
 
-class BookingList(generic.ListView):
-    model = Booking
-    template_name = "bookings.html"
+# class BookingList(generic.ListView):
+#     model = Booking
+#     template_name = 'bookings.html'
+
+class BookingView(generic.FormView):
+    form_class = AvailabilityForm
+    template_name = 'bookings.html'
+
+    
