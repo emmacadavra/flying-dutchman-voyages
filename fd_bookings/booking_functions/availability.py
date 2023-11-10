c0868049d8ebfe9dc49be3aed619d4aa313ba30e
Emma Cadavra <emmacadavra@gmail.com>
2023-11-10 16:37:16 +0000

    Add for loop inside check_availability

--- fd_bookings/booking_functions/availability.py
+++ fd_bookings/booking_functions/availability.py
@@ -5,4 +5,8 @@ from fd_bookings.models import Room, Booking
 def check_availability(room, booking_date):
     available_list = []
     booking_list = Booking.objects.filter(room=room)
-    
+    for booking in booking_list:
+        if booking_date == booking.booking_date:
+            available_list.append(False)
+        else:
+            available_list.append(True)
