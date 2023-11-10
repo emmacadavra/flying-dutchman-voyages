e1870837d46974b4f7506f2dda3107607771e1c3
Emma Cadavra <emmacadavra@gmail.com>
2023-11-10 13:14:28 +0000

    Amend Booking model

--- fd_bookings/models.py
+++ fd_bookings/models.py
@@ -21,14 +21,12 @@ class Room(models.Model):
 
 class Booking(models.Model):
     user = models.ForeignKey(User, on_delete=models.PROTECT)
-    # first_name = models.CharField(max_length=100)
-    # last_name = models.CharField(max_length=100)
     created_on = models.DateTimeField(auto_now_add=True)
     modified_on = models.DateTimeField(auto_now=True)
     booking_date = models.DateField()
+    room = models.ForeignKey(Room, on_delete=models.PROTECT)
     num_passengers = models.PositiveSmallIntegerField()
-    # event = choices?
     total_cost = models.DecimalField(decimal_places=2, max_digits=10)
 
     def __str__(self):
-        return f"Booking {self.id} created by {self.user.username} for {self.booking_date}"
+        return f"Booking {self.id} created by {self.user.username}. Room {self.room.category} {self.room.number} for {self.num_passengers} on {self.booking_date}."
