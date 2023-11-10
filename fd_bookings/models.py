ee832a02dcec880f8cac7c7b3b66371b4f4cd892
Emma Cadavra <emmacadavra@gmail.com>
2023-11-10 12:48:55 +0000

    Add Room model to models.py including 3 different categories

--- fd_bookings/models.py
+++ fd_bookings/models.py
@@ -3,6 +3,22 @@ from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
 
 
+class Room(models.Model):
+    ROOM_CATEGORIES = (
+        ('CAP', 'CAPTAIN QUARTERS'),
+        ('OCB', 'OFFICER CABIN'),
+        ('CRW', 'CREW BUNKS'),
+    )
+
+    category = models.CharField(max_length=3, choices=ROOM_CATEGORIES)
+    number = models.IntegerField()
+    beds = models.IntegerField()
+    capacity = models.IntegerField()
+
+    def __str__(self):
+        return f'{self.category} ({self.number}) with {self.beds} bed(s) for {self.capacity} passenger(s).'
+
+
 class Booking(models.Model):
     user = models.ForeignKey(User, on_delete=models.PROTECT)
     # first_name = models.CharField(max_length=100)
