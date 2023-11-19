72b59dde3d3373ebd26780f5c6c9bcbcebef47e7
Emma Cadavra <emmacadavra@gmail.com>
2023-11-19 20:42:50 +0000

    Paste part of RoomDetailView post function into get_available_rooms

--- fd_bookings/booking_functions/get_available_rooms.py
+++ fd_bookings/booking_functions/get_available_rooms.py
@@ -1,1 +1,14 @@
 from fd_bookings.models import Room
+
+
+def get_available_rooms(room_category, form):
+    room_list = Room.objects.filter(category=room_category)
+
+    if form.is_valid():
+            data = form.cleaned_data
+
+    available_rooms = []
+
+    for room in room_list:
+        if check_availability(room, data['booking_date']):
+            available_rooms.append(room)
