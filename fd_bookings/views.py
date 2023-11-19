bbd726b9c9c3000b5ca4fe6dd830d0a6312b921b
Emma Cadavra <emmacadavra@gmail.com>
2023-11-19 20:03:33 +0000

    Amend get_available_rooms_in_category to get_readable_categories

--- fd_bookings/views.py
+++ fd_bookings/views.py
@@ -45,20 +45,17 @@ class RoomDetailView(generic.View):
     def get(self, request, *args, **kwargs):
         room_category = kwargs.get('category', None)
         form = AvailabilityForm()
-        room_list = Room.objects.filter(category=room_category)
 
-        if len(room_list)>0:
-            room = room_list[0]
-            room_category = dict(room.ROOM_CATEGORIES).get(room.category, None)
-            context = {
-                'room_category': room_category,
-                'form': form,
-                'room': room,
-            }
-            return render(request, 'fd_bookings/room_detail.html', context)
-        else:
-# NEED TO UPDATE THIS TO BETTER RESPONSES/REDIRECTS
-            return HttpResponse('Room category does not exist.')
+        context = {
+            'room_category': room_category,
+            'form': form,
+            'room': room,
+        }
+    return render(request, 'fd_bookings/room_detail.html', context)
+    # else:
+    # # NEED TO UPDATE THIS TO BETTER RESPONSES/REDIRECTS
+    # return HttpResponse('Room category does not exist.')
+
 
     def post(self, request, *args, **kwargs):
         room_category = kwargs.get('category', None)
