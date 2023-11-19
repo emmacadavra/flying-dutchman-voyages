ae6a7ecbb00f22f890ef7ff939f573856cb92bc1
Emma Cadavra <emmacadavra@gmail.com>
2023-11-19 11:34:40 +0000

    Import Room model into get_rooms

--- fd_bookings/booking_functions/get_rooms.py
+++ fd_bookings/booking_functions/get_rooms.py
@@ -1,3 +1,6 @@
+from fd_bookings.models import Room
+
+
 def get_rooms():
     room = Room.objects.all()[0]
     room_categories = dict(room.ROOM_CATEGORIES)
@@ -8,3 +11,5 @@ def get_rooms():
         room_url = reverse('fd_bookings:RoomDetailView', kwargs={'category': room_category})
         
         room_list.append((room, room_url))
+    
+    return room_list
