cac2b030de441896968bdb7def75b1dc1816f54a
Emma Cadavra <emmacadavra@gmail.com>
2023-11-19 20:44:14 +0000

    Add if statement to get_available_rooms

--- fd_bookings/booking_functions/get_available_rooms.py
+++ fd_bookings/booking_functions/get_available_rooms.py
@@ -1,4 +1,5 @@
 from fd_bookings.models import Room
+from fd_bookings.booking_functions.check_availability import check_availability
 
 
 def get_available_rooms(room_category, form):
@@ -12,3 +13,8 @@ def get_available_rooms(room_category, form):
     for room in room_list:
         if check_availability(room, data['booking_date']):
             available_rooms.append(room)
+    
+    if len(available_rooms) > 0:
+         return available_rooms
+    else:
+         return None
