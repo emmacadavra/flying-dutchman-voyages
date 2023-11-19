63131628c3843e37d27577bbf22d5cd17903d688
Emma Cadavra <emmacadavra@gmail.com>
2023-11-19 21:56:48 +0000

    Comment out refactored code and paste in original code into RoomDetailView whilst trying to ascertain why there are issues with the data pulling through to room_detail

--- fd_bookings/views.py
+++ fd_bookings/views.py
@@ -4,6 +4,7 @@ from django.http import HttpResponse
 from django.urls import reverse, reverse_lazy
 from .models import Room, Booking
 from .forms import AvailabilityForm
+from fd_bookings.booking_functions.check_availability import check_availability
 from fd_bookings.booking_functions.get_room_category_urls import get_room_category_urls
 from fd_bookings.booking_functions.get_category_string import get_category_string
 from fd_bookings.booking_functions.get_available_rooms import get_available_rooms
@@ -48,41 +49,88 @@ class ViewBookingList(generic.ListView):
             return booking_list
         
 
+# class RoomDetailView(generic.View):
+#     # ADD DOCSTRING
+#     def get(self, request, *args, **kwargs):
+#         room_category = kwargs.get('category', None)
+#         category_string = get_category_string(room_category)
+#         form = AvailabilityForm()
+
+#         if category_string is not None:
+#         #  NEED TO INVESTIGATE HOW TO DISPLAY BEDS + CAPACITY AS IT IS NOT CURRENTLY WORKING
+#             context = {
+#                 'room_category': category_string,
+#                 'form': form,
+#                 'room': room,
+#             }
+#             return render(request, 'fd_bookings/room_detail.html', context)
+#         else:
+#     # NEED TO UPDATE THIS TO BETTER RESPONSES/REDIRECTS
+#             return HttpResponse('Room category does not exist.')
+
+
+#     def post(self, request, *args, **kwargs):
+#         room_category = kwargs.get('category', None)
+#         form = AvailabilityForm(request.POST)
+
+#         if form.is_valid():
+#             data = form.cleaned_data
+
+#         available_rooms = get_available_rooms(room_category, data['booking_date'], data['num_passengers'])
+
+#         if available_rooms is not None:
+#             booking = book_room(request, available_rooms[0], data['booking_date'], data['num_passengers'])
+
+# # NEED TO UPDATE THESE TO BETTER RESPONSES/REDIRECTS
+#             return HttpResponse(booking)
+#         else:
+#             return HttpResponse('This room is not available.')
+
 class RoomDetailView(generic.View):
-    # ADD DOCSTRING
     def get(self, request, *args, **kwargs):
         room_category = kwargs.get('category', None)
-        category_string = get_category_string(room_category)
         form = AvailabilityForm()
+        room_list = Room.objects.filter(category=room_category)
 
-        if category_string is not None:
-        #  NEED TO INVESTIGATE HOW TO DISPLAY BEDS + CAPACITY AS IT IS NOT CURRENTLY WORKING
+        if len(room_list)>0:
+            room = room_list[0]
+            room_category = dict(room.ROOM_CATEGORIES).get(room.category, None)
             context = {
-                'room_category': category_string,
+                'room_category': room_category,
                 'form': form,
+                'room': room,
             }
             return render(request, 'fd_bookings/room_detail.html', context)
         else:
-    # NEED TO UPDATE THIS TO BETTER RESPONSES/REDIRECTS
             return HttpResponse('Room category does not exist.')
-
 
     def post(self, request, *args, **kwargs):
         room_category = kwargs.get('category', None)
+        room_list = Room.objects.filter(category=room_category)
         form = AvailabilityForm(request.POST)
 
         if form.is_valid():
             data = form.cleaned_data
 
-        available_rooms = get_available_rooms(room_category, data['booking_date'], data['num_passengers'])
+        available_rooms = []
 
-        if available_rooms is not None:
-            booking = book_room(request, available_rooms[0], data['booking_date'], data['num_passengers'])
+        for room in room_list:
+            if check_availability(room, data['booking_date']):
+                available_rooms.append(room)
 
-# NEED TO UPDATE THESE TO BETTER RESPONSES/REDIRECTS
+        if len(available_rooms) > 0:
+            room = available_rooms[0]
+            booking = Booking.objects.create(
+                user = self.request.user,
+                room = room,
+                booking_date = data['booking_date'],
+                num_passengers = data['num_passengers']
+            )
+            booking.save()
             return HttpResponse(booking)
         else:
             return HttpResponse('This room is not available.')
+
 
 
 class CancelBooking(generic.DeleteView):
