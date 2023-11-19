d91014e6e324eb46184588c4cca53d95532aa5ca
Emma Cadavra <emmacadavra@gmail.com>
2023-11-19 11:32:22 +0000

    Define get_rooms in get_rooms.py (changed from room_list) and paste in code from RoomList view

---
+++ fd_bookings/booking_functions/room_list.py
@@ -0,0 +1,10 @@
+def get_rooms():
+    room = Room.objects.all()[0]
+    room_categories = dict(room.ROOM_CATEGORIES)
+    room_list = []
+
+    for room_category in room_categories:
+        room = room_categories.get(room_category)
+        room_url = reverse('fd_bookings:RoomDetailView', kwargs={'category': room_category})
+        
+        room_list.append((room, room_url))
