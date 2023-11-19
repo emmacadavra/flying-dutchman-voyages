70968e2d13a6aea03a8de431b418941fd34e99d6
Emma Cadavra <emmacadavra@gmail.com>
2023-11-19 19:22:38 +0000

    Experiment with booking_date validation, uncomment-out total_cost from Booking model

--- fd_bookings/forms.py
+++ fd_bookings/forms.py
@@ -8,6 +8,13 @@ class AvailabilityForm(forms.Form):
         ('SCBN', 'Single Cabin'),
         ('CRWH', 'Crew Hammocks'),
     )
-    
+
+    # def validate_sunday(value):
+    #     if value is not 6:
+    #         raise ValidationError(
+    #             ('%(value)s is not a valid day of departure'), params={'value': value},
+    #         )
+    # booking_date = forms.DateField(validators=[validate_sunday])
     booking_date = forms.DateField(required=True)
     num_passengers = forms.IntegerField(required=True)
+
--- fd_bookings/models.py
+++ fd_bookings/models.py
@@ -17,24 +17,21 @@ class Room(models.Model):
     beds = models.IntegerField()
     capacity = models.IntegerField()
     room_cost = models.DecimalField(decimal_places=2, max_digits=10, null=True)
-    # room_image = 
+    # room_image = CloudinaryField()
 
     def __str__(self):
         return f'{self.category} ({self.number}) with {self.beds} bed(s) for {self.capacity} passenger(s)'
 
 
 class Booking(models.Model):
+
     user = models.ForeignKey(User, on_delete=models.PROTECT)
     created_on = models.DateTimeField(auto_now_add=True)
     modified_on = models.DateTimeField(auto_now=True)
     booking_date = models.DateField()
-    # booking_date = models.DateField(validators=[validate_sun])
     room = models.ForeignKey(Room, on_delete=models.PROTECT, null=True)
     num_passengers = models.PositiveSmallIntegerField()
-    # total_cost = models.DecimalField(decimal_places=2, max_digits=10)
-
-    # def validate_sun(value):
-    #     if value is not 1
+    total_cost = models.DecimalField(decimal_places=2, max_digits=10, default=0)
 
     def __str__(self):
         return f"Booking {self.id} created by {self.user.username}. Room {self.room} on {self.booking_date}"
