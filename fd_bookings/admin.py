bc7d87a1164196200b425073cbc58e772c7e3c02
Emma Cadavra <emmacadavra@gmail.com>
2023-11-25 13:07:34 +0000

    Begin refactoring code in admin.py

--- fd_bookings/admin.py
+++ fd_bookings/admin.py
@@ -2,5 +2,19 @@ from django.contrib import admin
 from .models import Room, Booking
 
 
-admin.site.register(Room)
-admin.site.register(Booking)
+# admin.site.register(Room)
+# admin.site.register(Booking)
+
+@admin.register(Room)
+class RoomAdmin(admin.ModelAdmin):
+
+    list_display =
+    search_fields =
+    list_filter =
+
+@admin.register(Booking)
+class BookingAdmin(admin.ModelAdmin):
+
+    list_display =
+    search_fields =
+    list_filter =
