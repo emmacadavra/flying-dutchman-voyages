6dffd736258248a6a761e763057055fc517dc6f4
Emma Cadavra <emmacadavra@gmail.com>
2023-11-25 13:11:33 +0000

    Add list_display and list_filter to RoomAdmin

--- fd_bookings/admin.py
+++ fd_bookings/admin.py
@@ -8,9 +8,8 @@ from .models import Room, Booking
 @admin.register(Room)
 class RoomAdmin(admin.ModelAdmin):
 
-    list_display =
-    search_fields =
-    list_filter =
+    list_display = ('category', 'number', 'beds', 'capacity')
+    list_filter = ('category', 'beds', 'capacity')
 
 @admin.register(Booking)
 class BookingAdmin(admin.ModelAdmin):
